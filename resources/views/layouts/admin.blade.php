c3a11fea46f60fabb8b84ead26603fb15295765f
Bajomo David <bajomodavid@gmail.com>
2020-01-12 10:53:46 +0100

    Updated Board Dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -225,7 +225,7 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">State Revenue Boards</h5>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Revenue Boards</h5>
                         <span class="h2 font-weight-bold mb-0">{{number_format($agencies)}}</span>
                     </div>
                     <div class="col-auto">
