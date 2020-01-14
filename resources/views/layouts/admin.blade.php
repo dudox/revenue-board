5ae8e645a25b9b471be379f14cd896f0942dfe9e
Bajomo David <bajomodavid@gmail.com>
2020-01-14 14:20:24 +0100

    Updated Board Dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -291,7 +291,7 @@
     <div class="container-fluid mt--7">
         @yield('content')
       <!-- Footer -->
-      <footer class="footer" style="bottom: 3%;">
+      <footer class="footer" style="position: fixed; bottom: 3%;">
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
             <div class="copyright text-center text-xl-left text-muted">
