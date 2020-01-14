7e2e8972e7380f0c716b617077f033d143bddcbd
Bajomo David <bajomodavid@gmail.com>
2020-01-14 13:44:34 +0100

    Updated Board Dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
     <div class="container mt--5">
       <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
-          <div class="card bg-primary">
+          <div class="card bg-primary" style="border: none;">
             <div class="card-body px-lg-5 py-lg-5">
               <form role="form" method="POST" action="{{route('login')}}">
                 @csrf
@@ -101,7 +101,7 @@
         </div>
       </div>
     </div>
-    <footer style="position:fixed; left: 2%; bottom: 0; width: 100%;">
+    <footer style="position:fixed; left: 2%; bottom: 2%; width: 100%;">
       <div class="">
         <div class="row">
           <div class="col-xl-6" style="">
