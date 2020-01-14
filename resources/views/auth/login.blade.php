cda19eb87f05d2709dd429577a561fb6f026740e
Bajomo David <bajomodavid@gmail.com>
2020-01-14 13:22:49 +0100

    Updated Board Dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,46 +36,18 @@
 <body class="bg-gradient-primary">
   <div class="main-content">
     <!-- Navbar -->
-    <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
-      <div class="container px-3">
-        <a class="navbar-brand" href="../index.html">
-
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-collapse-main">
-          <!-- Collapse header -->
-          <div class="navbar-collapse-header d-md-none">
-            <div class="row">
-              <div class="col-6 collapse-brand">
-                <a href="../index.html">
-                  <img src="../assets/img/brand/blue.png">
-                </a>
-              </div>
-              <div class="col-6 collapse-close">
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
     <!-- Header -->
-    <div class="header bg-gradient-primary py-1 py-lg-8">
-      <div class="container">
-        <div class="header-body text-center mb-3">
-          <div class="row justify-content-center">
-            <div class="col-lg-5 col-md-6">
-                <img src="../assets/img/brand/blue.svg" width="150em" height="150em" />
-              <h3 class="text-white">States internal revenue services reporting dashboard</h3>
-            </div>
-          </div>
+    <div class="container">
+    <div class="header-body text-center mb-4">
+        <div class="row justify-content-center">
+        <div class="col-lg-12 col-md-12">
+            <img src="../assets/img/brand/blue.svg" width="150em" height="150em" />
+            <h3 class="text-white">States Internal Revenue Services Reporting Dashboard</h3>
         </div>
-      </div>
+        </div>
+    </div>
+    </div>
+    <div class="header bg-gradient-primary py-1 py-lg-7">
       {{-- <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
@@ -135,11 +107,11 @@
         </div>
       </div>
     </div>
-    <footer class="py-4">
-      <div class="container">
-        <div class="row align-items-center justify-content-xl-between">
-          <div class="col-xl-12">
-            <div class="copyright text-center text-xl-left text-white">
+    <footer style="position:fixed; left: 2%; bottom: 0; width: 100%;">
+      <div class="">
+        <div class="row">
+          <div class="col-xl-6" style="">
+            <div class="copyright text-xl-left text-white">
               © {{date("Y", time())}} <a href="https://www.mayapro1.com" class="font-weight-bold ml-1 text-white" target="_blank">Mayapro1 Limited</a>
             </div>
           </div>
