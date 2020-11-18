b4dcf0e20efa788f2b32d0a4d4b425e527aab847
dudox <snub.com@gmail.com>
2020-11-18 14:57:03 +0100

    changed copyright to smartsahara

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -296,7 +296,7 @@
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
             <div class="copyright text-center text-xl-left text-muted">
-              &copy; {{date("Y", time())}} <a href="https://www.mayapro1.com" class="font-weight-bold ml-1" target="_blank">Mayapro1 Limited</a>
+              &copy; {{date("Y", time())}} <a href="https://www.smartsahara.com" class="font-weight-bold ml-1" target="_blank">SmartSahara</a>
             </div>
           </div>
         </div>
